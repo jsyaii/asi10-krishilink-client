5933cd7e63bd143801a19cf1e2c63ce10fb91621
jsyaii <jasiahasan1703@gmail.com>
2025-11-11 18:13:35 +0600

    log in  create

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
 
-const LogIn = () => {
+
+import React, { use, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router';
+import { AuthContext } from '../../provider/AuthProvider';
+
+
+const Login = () => {
+ const [error, setError] = useState("");
+  const { signIn } = use(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  // console.log(location);
+  
+
     return (
-        <div>
-            <h1>log in</h1>
-        </div>
+         <div className="flex justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+        <h2 className="font-semibold text-2xl text-center">
+          Login your account
+        </h2>
+       
+        <form onSubmit={handleLogin} className="card-body">
+          <fieldset className="fieldset">
+            {/* email  */}
+           
+            {/* passowrd  */}
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+            
+            <p className="text-center mt-2">
+  Forgot your password?{" "}
+  <Link className="text-blue-500" to="/auth/forgot-password">
+    Reset here
+  </Link>
+</p>
+
+            {error && <p className="text-red-400 text-xs">{error}</p>}
+
+            <button type="submit" className="btn btn-neutral mt-4">
+              Login
+            </button>
+            <p className="font-semibold text-center pt-5">
+              Dont’t Have An Account ?{" "}
+              <Link className="text-secondary" to="/auth/signup">
+                Sign Up
+              </Link>
+            </p>
+          </fieldset>
+        </form>
+      </div>
+    </div>
     );
 }
 
-export default LogIn;
+export default Login;
