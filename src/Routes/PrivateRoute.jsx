59d567649926e621251007f2735b1921c6c3e5e7
jsyaii <jasiahasan1703@gmail.com>
2025-11-11 18:32:48 +0600

    private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React, { use } from "react";
+
+import { Navigate, useLocation } from "react-router";
+import Loading from "../pages/Loading/Loading";
+import { AuthContext } from "./AuthProvider";
+
+
+
+const PrivateRoute = ({ children }) => {
+  
+};
+
+export default PrivateRoute;
