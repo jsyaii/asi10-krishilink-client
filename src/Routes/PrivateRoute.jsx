5c71025f4718e8bc88ab257870bc4ea5690856ab
jsyaii <jasiahasan1703@gmail.com>
2025-11-11 18:33:47 +0600

    private route complete

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -7,7 +7,22 @@ import { AuthContext } from "./AuthProvider";
 
 
 const PrivateRoute = ({ children }) => {
-  
+  const { user, loading } = use(AuthContext);
+  //   console.log(user);
+
+  const location = useLocation();
+//   console.log(location);
+
+  if (loading) {
+    return <Loading></Loading>
+
+  }
+
+  if (user && user?.email) {
+    return children;
+  }
+  return <Navigate state={location.pathname} to="/auth/login"></Navigate>;
+
 };
 
 export default PrivateRoute;
